0540a2975a256a09d16d2ca7caa256c13395f444
vanessa082 <wahvanessa22@gmail.com>
2024-01-30 18:30:26 +0100

    first commit with recipe.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import  { useEffect, useState } from 'react';
+import './App.css'; 
+import Recipe from './Recipe/Recipe';
 
-function App() {
-  
-  const APP_ID = 
+const App = () => {
+  const APP_ID = "0fcfe5c9"; // Add your Application ID here
+  const APP_KEY = "70866636d35aba875297493787c4f977"; // Add your Application Key here
+  const [recipes, setRecipes] = useState([]); 
+  const [search, setSearch] = useState(""); 
+  const [query, setQuery] = useState("chicken"); 
 
+  useEffect(() => {
+    getRecipes(); 
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  const updateSearch = e => { 
+    setSearch(e.target.value); 
+  };
+
+  const getSearch = e => { 
+    e.preventDefault(); 
+    setQuery(search); 
+    setSearch(""); 
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App"> 
+      <form className="search-form" onSubmit={getSearch} > 
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} /> 
+        <button className="search-button" type="submit"> 
+          Search 
+        </button> 
+      </form> 
+      <div className="recipes"> 
+        {recipes.map(recipe => (
+          <Recipe 
+            key={recipe.recipe.label} 
+            title={recipe.recipe.label} 
+            calories={recipe.recipe.calories} 
+            image={recipe.recipe.image} 
+            ingredients={recipe.recipe.ingredients} 
+          /> 
+        ))} 
+      </div> 
+    </div> 
+  ); 
+};
 
-export default App
+export default App;
