dfea46e6254f7607117a9d61183eca08503d0810
vanessa <wahvanessa22@gmail.com>
2024-04-10 23:44:23 +0100

    added usestate hook to manage the form dispay and rrepresentation giving the the add recipe form a prop ...

--- src/Components/Recipeform/Recipeform.jsx
+++ src/Components/Recipeform/Recipeform.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-const AddRecipeForm = () => {
+const AddRecipeForm = ({onAddRecipe}) => {
   const [mealName, setMealName] = useState("");
   const [mealCategory, setMealCategory] = useState("");
   const [mealImage, setMealImage] = useState("");
@@ -7,8 +7,29 @@ const AddRecipeForm = () => {
   const [mealMeasurements, setMealMeasurement] = useState("");
   const [mealInstructions, setMealInstructions] = useState("");
 
-  const handleSubmit = (e) =>{
-    e.preventDefault
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      !mealName.trim() ||
+      !mealCategory.trim() ||
+      !mealImage.trim() ||
+      !mealIngredients.trim() ||
+      !mealMeasurements.trim() ||
+      !mealInstructions.trim()
+    )
+      return;
+
+    const newRecipe = {
+      name: mealName,
+      category: mealCategory,
+      image: mealImage,
+      ingredients: mealIngredients,
+      measurement: mealMeasurements,
+      instructions: mealInstructions,
+    };
+
+    onAddRecipe(newRecipe)
+  };
   return;
 };
